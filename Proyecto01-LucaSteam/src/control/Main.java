f39eabd9be5f0ec5f15a57ac897c263d80f8cafd
Patricia <grpatri9@gmail.com>
2021-06-15 10:43:54 +0200

    Escribiendo en el Main

--- Proyecto01-LucaSteam/src/control/Main.java
+++ Proyecto01-LucaSteam/src/control/Main.java
@@ -3,7 +3,10 @@ package control;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Prueba git. ");
+		System.out.println(" ");
+		System.out.println("Prueba para git. ");
 
 	}
 
