35b1775af82b41ca10e898e4936b9fb41d0ab041
Jennifer <perezmartinezjennifer05@gmail.com>
2021-06-15 19:40:48 +0200

    Leer Fichero

--- Proyecto01-LucaSteam/src/control/Main.java
+++ Proyecto01-LucaSteam/src/control/Main.java
@@ -5,25 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		
-		String LiarlaParda = new String();
-		System.out.println("LiarlaParda");
-
-
-
-		// TODO Auto-generated method stub
-		
-		// comentariosara
-
-	
-		System.out.println("Prueba git. ");
-		System.out.println(" ");
-		System.out.println("Prueba para git. ");
-
-
-
-		//Comentario MariaC
-		
-		//Imprimir.Maria();
 	}
 
 }
