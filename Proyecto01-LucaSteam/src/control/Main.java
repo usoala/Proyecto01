14975ba1f61f3fcf076e55e64bcabc185998ce15
Jennifer <perezmartinezjennifer05@gmail.com>
2021-06-15 10:47:23 +0200

    Se va a liar

--- Proyecto01-LucaSteam/src/control/Main.java
+++ Proyecto01-LucaSteam/src/control/Main.java
@@ -3,8 +3,8 @@ package control;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Test.imprimir();
+		LiarlaParda Objeto = new LiarlaParda();
 	}
 
 }
