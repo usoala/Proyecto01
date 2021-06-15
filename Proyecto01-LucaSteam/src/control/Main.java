b831d458f6fd527f1ff66d694bd77e1b8eed528d
Jennifer <perezmartinezjennifer05@gmail.com>
2021-06-15 10:59:09 +0200

    Se va a liar

--- Proyecto01-LucaSteam/src/control/Main.java
+++ Proyecto01-LucaSteam/src/control/Main.java
@@ -3,8 +3,9 @@ package control;
 public class Main {
 
 	public static void main(String[] args) {
-		Test.imprimir();
-		LiarlaParda Objeto = new LiarlaParda();
+		
+		String LiarlaParda = new String();
+		System.out.println("LiarlaParda");
 	}
 
 }
