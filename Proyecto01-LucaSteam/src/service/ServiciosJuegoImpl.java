4d6f4b3dec8da916f889d64cedea15fb9cffe59b
SaraSilvoLopez <you@example.com>
2021-06-16 13:22:43 +0200

    metodo serializar capa servicio y capa datos

--- Proyecto01-LucaSteam/src/service/ServiciosJuegoImpl.java
+++ Proyecto01-LucaSteam/src/service/ServiciosJuegoImpl.java
@@ -6,20 +6,18 @@ import exception.JuegoException;
 import model.Juego;
 
 /**
- * @ClassName ServiciosJuego
- * Implementación de la Interface ServiciosJuego
+ * @ClassName ServiciosJuego Implementación de la Interface ServiciosJuego
  *
- * @author María Castro, Patricia García, Usoa Larrarte,
- * Jennifer Pérez y Sara Silvo
+ * @author María Castro, Patricia García, Usoa Larrarte, Jennifer Pérez y Sara
+ *         Silvo
  *
  * @date 15 jun. 2021
  * 
  * @version 1.0
  */
 public class ServiciosJuegoImpl implements ServiciosJuego {
-	
+
 	private CatalogoJuegos catalogo = new CatalogoJuegosImpl();
-	
 
 	/**
 	 * Método getter del atributo catalogo
@@ -33,7 +31,7 @@ public class ServiciosJuegoImpl implements ServiciosJuego {
 	/**
 	 * Método setter del atributo catalogo
 	 *
-	 * @param catalogo to set catalogo 
+	 * @param catalogo to set catalogo
 	 */
 	public void setCatalogo(CatalogoJuegos catalogo) {
 		this.catalogo = catalogo;
@@ -42,22 +40,21 @@ public class ServiciosJuegoImpl implements ServiciosJuego {
 	/**
 	 * Implementación del método altaJuego
 	 *
-	 * @return 
+	 * @return
 	 * @throws JuegoException
 	 */
-	
-	
+
 	public boolean altaJuego() throws JuegoException {
 		Juego juego = new Juego();
 		juego.crearJuego();
 		return this.altaJuego(juego);
 	}
-	
+
 	/**
 	 * Sobrescritura del método altaJuego
 	 *
 	 * @param juego
-	 * @return 
+	 * @return
 	 * @throws JuegoException
 	 */
 	public boolean altaJuego(Juego juego) throws JuegoException {
@@ -72,5 +69,21 @@ public class ServiciosJuegoImpl implements ServiciosJuego {
 		catalogo.listarJuegos();
 	}
 
+	/**
+	 * Implementación del método serializarCatalogoJuegosImpl
+	 *
+	 */
+	public void serializarCatalogoJuegosImpl() {
+		catalogo.serializarCatalogoJuegosImpl("CSVDatosSelecPorComas.csv");
+
+	}
+
+	/**
+	 * Implementación del método deSerializarCatalogoJuegosImpl
+	 *
+	 */
+	public void deSerializarCatalogoJuegosImpl() {
+		catalogo.deSerializarCatalogoJuegosImpl("CSVDatosSelecPorComas.csv");
+	}
 
 }
